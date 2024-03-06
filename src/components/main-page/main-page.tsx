ad2c16154dac5c4ec416d7537e04b65d31684b91
Victor Alves <victorhugo_sp4@hotmail.com>
2024-03-05 21:02:43 -0300

    add use client

--- src/components/main-page/main-page.tsx
+++ src/components/main-page/main-page.tsx
@@ -1,10 +1,10 @@
+"use client"
 import PokemonCard from "../pokemon-card/pokemon-card"
 
 export default function MainPage(){
     return(
         <div>
             <h1>Pokédex</h1>
-            <h3>Gen 1</h3>
             <PokemonCard/>
         </div>
     )
