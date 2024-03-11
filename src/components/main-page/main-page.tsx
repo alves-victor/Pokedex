c73d93b0336d1801ab338651525ef8dc5b0e9ca3
Victor Alves <victorhugo_sp4@hotmail.com>
2024-03-11 20:50:11 -0300

    add função por props para comunicação entre componentes

--- src/components/main-page/main-page.tsx
+++ src/components/main-page/main-page.tsx
@@ -1,11 +1,19 @@
 "use client"
+import { useState } from "react"
 import PokemonCard from "../pokemon-card/pokemon-card"
 
 export default function MainPage(){
+    const [pokeId, setPokeId] = useState(0)
+
+    function ShowModal(id: number){
+        setPokeId(id)
+    }
+
+
     return(
         <div>
             <h1>Pokédex</h1>
-            <PokemonCard/>
+            <PokemonCard ShowModal={ShowModal}/>
         </div>
     )
 }
