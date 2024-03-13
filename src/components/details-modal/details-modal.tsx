35bc9085e0fb0c04e133965dfc19a7730400149d
Victor Alves <victorhugo_sp4@hotmail.com>
2024-03-13 20:54:26 -0300

    construção inicial da parte descritiva do modal

--- src/components/details-modal/details-modal.tsx
+++ src/components/details-modal/details-modal.tsx
@@ -11,10 +11,33 @@ export default function DetailsModal(props: any){
                 <section className="modal-section">
                     <Image className="close-btn" onClick={() => props.invisible("invisible")} src={close} alt="close button"></Image>
                     <img className="poke-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="a" />
-                    <section className="name-section">
+                    <section className="name-section section-padding">
                         <span>bulbassaur</span>
                         <span>#1</span>
                     </section>
+                </section>
+
+                <section className="modal-section">
+                    <p className="item">types</p>
+                    <section className="type-section section-padding item">
+                        <ol className="pokemon-types">
+                            <li className="type grass">grass</li>
+                            <li className="type poison">poison</li>
+                        </ol>
+                    </section>
+
+                    <p className="item">abilities</p>
+                    <ul className="abilities item">
+                        <li>overgrow</li>
+                        <li>chlorophyll</li>
+                    </ul>
+                    
+                    <p className="item">details</p>
+                    <section className="details section-padding item">
+                        <span>heigth: 70cm</span>
+                        <span>weigth: 69cm</span>
+                    </section>
+
                 </section>
                 
             </main>
