c563584c5c4d2788251341dc73b0c8dd2e635de7
Victor Alves <victorhugo_sp4@hotmail.com>
2024-03-13 00:03:07 -0300

    ajuste do topo do details-modal

--- src/components/details-modal/details-modal.tsx
+++ src/components/details-modal/details-modal.tsx
@@ -1,11 +1,23 @@
 import "./style.css"
+import Image from "next/image"
+import close from "../../../public/close.svg"
 
 export default function DetailsModal(props: any){
 
 
     return(
         <div className={"modal " + props.class}>
-            <button onClick={() => props.invisible("invisible")}>teste2</button>
+            <main className="modal-main">
+                <section className="modal-section">
+                    <Image className="close-btn" onClick={() => props.invisible("invisible")} src={close} alt="close button"></Image>
+                    <img className="poke-img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="a" />
+                    <section className="name-section">
+                        <span>bulbassaur</span>
+                        <span>#1</span>
+                    </section>
+                </section>
+                
+            </main>
         </div>
     )
 }
