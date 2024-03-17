993372df86624c1b54061dae92845e7d880ad931
Victor Alves <victorhugo_sp4@hotmail.com>
2024-03-17 20:23:32 -0300

    mudança no import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import MainPage from "@/components/main-page/main-page";
+import PokemonCard from "@/components/pokemon-card/pokemon-card";
 import Style from "@/app/page.module.css"
 
 export default function Home() {
   return (
     <main className={Style.main}>
-      <MainPage/>
+      <PokemonCard/>
     </main>
   );
 }
