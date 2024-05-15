65ef2259defdebb52f7cc37f0ce70de645069483
Victor Alves <victorhugo_sp4@hotmail.com>
2024-05-15 18:04:25 -0300

    teste de implementação

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
+};
 
 export default nextConfig;
-
-module.exports = {
-    experimental: {
-      missingSuspenseWithCSRBailout: false,
-    },
-  }
